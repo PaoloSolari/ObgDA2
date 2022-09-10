8020c8c96444b250552f1a8d55bdaa45a15e95a8
Gabriel Jaime <g.jaimelarrouyet@gmail.com>
2022-09-10 13:24:48 -0300

    Estructuración de paqeutes a un patrón de diseño de 'Domain Model' lineal.

---
+++ obg.DataAccess.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace obg.DataAccess.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
