caddd50c8dee574f4fc50467ffd5aa699610d784
Paolo Solari <oloap99@gmail.com>
2022-11-08 19:15:17 -0300

    Tip added to resolved exporter variable parameters.

--- Backend/obg.JSONExporter/JSON.cs
+++ Backend/obg.JSONExporter/JSON.cs
@@ -10,10 +10,13 @@ namespace obg.JSONExporter
     public class JSON : IExporter
     {
         public string GetName()
-        {
+        { 
             return "JSON";
         }
 
+        //  deben "exigirle" a cada exportador que "les diga" qué parámetros necesita.
+        // manejarlos con un mapa string -> string (donde la clave es el nombre del parámetro a mostrar en la UI y el valor es el valor real que el usuario termina definiendo).
+        // Dictionary<string, string>
         public void ExportData(List<Medicine> medicinesToExport, string path)
         {
             string jsonString = JsonSerializer.Serialize(medicinesToExport);
